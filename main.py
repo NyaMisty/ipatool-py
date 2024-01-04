949b730e1a248879f3037083d3909de0f1394f2a
Misty <gyc990326@gmail.com>
2024-01-05 01:27:35 +0800

    Add a check to avoid partial download due to aborted connection

--- main.py
+++ main.py
@@ -41,18 +41,24 @@ def downloadFile(url, outfile):
         totalLen = int(r.headers.get('Content-Length', '0'))
         downLen = 0
         r.raise_for_status()
-        with open(outfile, 'wb') as f:
-            lastLen = 0
-            for chunk in r.iter_content(chunk_size=1 * 1024 * 1024):
-                # If you have chunk encoded response uncomment if
-                # and set chunk_size parameter to None.
-                # if chunk:
-                f.write(chunk)
-                downLen += len(chunk)
-                if totalLen and downLen - lastLen > totalLen * 0.05:
-                    logger.info("Download progress: %3.2f%% (%5.1fM /%5.1fM)" % (
-                    downLen / totalLen * 100, downLen / 1024 / 1024, totalLen / 1024 / 1024))
-                    lastLen = downLen
+        try:
+            with open(outfile, 'wb') as f:
+                lastLen = 0
+                for chunk in r.iter_content(chunk_size=1 * 1024 * 1024):
+                    # If you have chunk encoded response uncomment if
+                    # and set chunk_size parameter to None.
+                    # if chunk:
+                    f.write(chunk)
+                    downLen += len(chunk)
+                    if totalLen and downLen - lastLen > totalLen * 0.05:
+                        logger.info("Download progress: %3.2f%% (%5.1fM /%5.1fM)" % (
+                        downLen / totalLen * 100, downLen / 1024 / 1024, totalLen / 1024 / 1024))
+                        lastLen = downLen
+        finally:
+            if downLen != totalLen: # ensure no partial downloaded files exists
+                os.unlink(outfile)
+        if downLen != totalLen:
+            raise Exception("failed to completely download the IPA file")
 
     return outfile
 
